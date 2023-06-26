b8493fd7e05f5328f30fad1028c66af3b8de5ebb
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-06-26 20:11:44 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,6 @@
 function post (){
   const form = document.getElementById("form");
-  form.addEventListener("submit", (e) => {  //ルーティングに送るリクエストを書いていくよ
+  form.addEventListener("submit", (e) => {  //コントローラーに送るリクエストを書いていくよ
       e.preventDefault();
     const formData = new FormData(form);  //フォームからの値を格納
     const XHR = new XMLHttpRequest();    //新しくオブジェクトを生成  サーバーとHTTP通信を行うために必要です お手紙のあて名書きみたいな
